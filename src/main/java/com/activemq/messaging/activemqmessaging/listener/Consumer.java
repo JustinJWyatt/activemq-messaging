30e9cb6b756afec8aeb1bf7cb59004326272d910
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-30 13:42:48 -0600

    added consumer

---
+++ src/main/java/com/activemq/messaging/activemqmessaging/listener/Consumer.java
@@ -0,0 +1,12 @@
+package com.activemq.messaging.activemqmessaging.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+    @JmsListener(destination = "standalone.queue")
+    public void consume(String message){
+        System.out.println("Received message: " + message);
+    }
+}
